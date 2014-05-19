80f93a2970c7d1f7a417ef1cf04e8f61e53e6848
Rijan de Nysschen <rdenysschen@ska.ac.za>
2014-05-19 14:52:48 +0200

    Add sensorlib

---
+++ sensorlib.c
@@ -0,0 +1,20 @@
+#include "sensorlib.h"
+
+#define SENSOR_CONF_PATH	NULL
+
+int sensorlib_load(void)
+{
+	int ret = -1;
+
+	ret = sensors_init(SENSOR_CONF_PATH);
+	if (ret) {
+		return -1;
+	}
+
+	return ret;
+}
+
+void sensorlib_unload(void)
+{
+	sensors_cleanup();
+}
---
+++ sensorlib.h
@@ -0,0 +1,20 @@
+#ifndef _SENSOR_LIB_H
+#define _SENSOR_LIB_H
+
+#include "lib/sensors.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int sensorlib_load(void);
+
+void sensorlib_unload(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SENSOR_LIB_H */
+
+
