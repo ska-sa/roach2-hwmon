5ead498d92415918cd35ef033be694e72d543627
Rijan de Nysschen <rdenysschen@ska.ac.za>
2014-06-02 14:59:32 +0200

    Added roach sensor lookup table.

--- log.c
+++ log.c
@@ -10,31 +10,69 @@
 #include <unistd.h>
 #include "log.h"
 
-struct descLookup {
+struct chipLookup {
 	char *chip;
 	char *name;
+	char *sensor;
 	char *desc;
+	char *unit;
+};
+
+static const struct chipLookup table[] = {
+		{"chip1", "name1", "sensor1", "desc1", "unit1"},
+		{"chip2", "name2", "sensor2", "desc2", "unit2"},
+		{"ad7414-i2c-0-4c", "temp1", "temp.inlet", "inlet temp", "millidegrees"},
+		{"ad7414-i2c-0-4e", "temp1", "temp.outlet", "outlet temp", "millidegrees"},
+		{"max1668-i2c-0-18", "temp1", "temp.ambient", "ambient temp", "millidegrees"},
+		{"max1668-i2c-0-18", "temp2", "temp.ppc", "ambient temp", "millidegrees"},
+		{"max1668-i2c-0-18", "temp3", "temp.fpga", "ambient temp", "millidegrees"},
+		{"max16071-i2c-0-50", "in0", "voltage.1v0", "1v voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in1", "voltage.1v5", "1v5 voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in2", "voltage.1v8", "1v8 voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in3", "voltage.2v5", "2v5 voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in4", "voltage.3v3", "3v3 voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in5", "voltage.5v", "5v voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in6", "voltage.12v", "12v voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "in7", "voltage.3v3aux", "3v3 aux voltage rail", "millivolts"},
+		{"max16071-i2c-0-50", "curr1", "current.12v", "12v rail current", "milliamps"},
+		{"max16071-i2c-0-51", "in0", "current.3v3", "1v voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in1", "current.2v5", "1v5 voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in2", "current.1v8", "1v8 voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in3", "current.1v5", "2v5 voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in4", "current.1v0", "3v3 voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in5", "ppc.sys.error", "5v voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in6", "voltage.5vaux", "5v aux voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "curr1", "current.5v", "5v rail current", "milliamps"},
+		{"max6650-i2c-0-48", "fan1", "fan.fpga", "fpga fan", "rpm"},
+		{"max6650-i2c-0-4b", "fan1", "fan.chs0", "chassis 0 fan", "rpm"},
+		{"max6650-i2c-0-1b", "fan1", "fan.chs1", "chassis 1 fan", "rpm"},
+		{"max6650-i2c-0-1f", "fan1", "fan.chs2", "chassis 2 fan", "rpm"},
+		{NULL, NULL, NULL, NULL, NULL}
 };
 
 static struct katcl_line *k = NULL;
 static char *app = "roach2hwmon";
 
-static char *getDescription(const char *chip, char *name)
+int getAttributes(const char *chip, char *name, char *sensor,
+							char *description, char *unit)
 {
 	int i = 0;
-	struct descLookup table[] = {
-			{"chip1", "name1", "desc1"},
-			{"chip2", "name2", "desc2"}
-	};
-	int numRows = 2; /// \todo determine at runtime?
 
-	for (i = 0; i < numRows; i++) {
+	while (table[i].chip != NULL) {
 		if (!(strcmp(chip, table[i].chip) && strcmp(name, table[i].name))) {
-			return table[i].desc;
+			sensor = table[i].sensor;
+			description = table[i].desc;
+			unit = table[i].unit;
+			return 0;
 		}
+		i++;
 	}
 
-	return "description";
+	sensor = name;
+	description = "no description";
+	unit = "unspecified";
+
+	return 1;
 }
 
 int log_init(void)
@@ -98,16 +136,22 @@ int log_sensorlist(void)
 int log_addsensor(const char *chip, char *name, double min, double max)
 {
 	int ret = 0;
+	char namebuff[256];
+	char descbuff[256];
+	char unitbuff[256];
 
 	if (k == NULL) {
 		return -1;
 	}
 
+	/* populate sensor attributes */
+	getAttributes(chip, name, namebuff, descbuff, unitbuff);
+
 	/* register sensor(s) */
 	ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-list");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, name);
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, getDescription(chip, name));
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "millidegrees");
+	ret += append_string_katcl(k, KATCP_FLAG_STRING, namebuff);
+	ret += append_string_katcl(k, KATCP_FLAG_STRING, descbuff);
+	ret += append_string_katcl(k, KATCP_FLAG_STRING, unitbuff);
 	ret += append_string_katcl(k, KATCP_FLAG_STRING, "integer");
 
 	/* multiply with a 1000 since our units are in milli's */
