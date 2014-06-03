0b74e330d5bfe3c109c0e2516e6db0381fb21296
Rijan de Nysschen <rdenysschen@ska.ac.za>
2014-06-03 10:33:25 +0200

    Fixed sensor mapping table, added timestamp for sensor-status logs.

--- log.c
+++ log.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include "log.h"
 
 struct chipLookup {
@@ -19,14 +20,15 @@ struct chipLookup {
 };
 
 static const struct chipLookup table[] = {
-		{"chip1", "name1", "sensor1", "desc1", "unit1"},
-		{"chip2", "name2", "sensor2", "desc2", "unit2"},
+		/* Adding sensor mappings
+           {"chip1", "name1", "sensor1", "desc1", "unit1"},
+		   {"chip2", "name2", "sensor2", "desc2", "unit2"}, */
 		{"acpitz-virtual-0", "temp1", "hp.temp", "hp temp sensor", "milidegrees"},
 		{"ad7414-i2c-0-4c", "temp1", "temp.inlet", "inlet temp", "millidegrees"},
 		{"ad7414-i2c-0-4e", "temp1", "temp.outlet", "outlet temp", "millidegrees"},
 		{"max1668-i2c-0-18", "temp1", "temp.ambient", "ambient temp", "millidegrees"},
-		{"max1668-i2c-0-18", "temp2", "temp.ppc", "ambient temp", "millidegrees"},
-		{"max1668-i2c-0-18", "temp3", "temp.fpga", "ambient temp", "millidegrees"},
+		{"max1668-i2c-0-18", "temp2", "temp.ppc", "ppc temp", "millidegrees"},
+		{"max1668-i2c-0-18", "temp3", "temp.fpga", "fpga temp", "millidegrees"},
 		{"max16071-i2c-0-50", "in0", "voltage.1v0", "1v voltage rail", "millivolts"},
 		{"max16071-i2c-0-50", "in1", "voltage.1v5", "1v5 voltage rail", "millivolts"},
 		{"max16071-i2c-0-50", "in2", "voltage.1v8", "1v8 voltage rail", "millivolts"},
@@ -36,12 +38,11 @@ static const struct chipLookup table[] = {
 		{"max16071-i2c-0-50", "in6", "voltage.12v", "12v voltage rail", "millivolts"},
 		{"max16071-i2c-0-50", "in7", "voltage.3v3aux", "3v3 aux voltage rail", "millivolts"},
 		{"max16071-i2c-0-50", "curr1", "current.12v", "12v rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in0", "current.3v3", "1v voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "in1", "current.2v5", "1v5 voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "in2", "current.1v8", "1v8 voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "in3", "current.1v5", "2v5 voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "in4", "current.1v0", "3v3 voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "in5", "ppc.sys.error", "5v voltage rail", "millivolts"},
+		{"max16071-i2c-0-51", "in0", "current.3v3", "1v rail current", "milliamps"},
+		{"max16071-i2c-0-51", "in1", "current.2v5", "1v5 rail current", "milliamps"},
+		{"max16071-i2c-0-51", "in2", "current.1v8", "1v8 rail current", "milliamps"},
+		{"max16071-i2c-0-51", "in3", "current.1v5", "2v5 rail current", "milliamps"},
+		{"max16071-i2c-0-51", "in4", "current.1v0", "3v3 rail current", "milliamps"},
 		{"max16071-i2c-0-51", "in6", "voltage.5vaux", "5v aux voltage rail", "millivolts"},
 		{"max16071-i2c-0-51", "curr1", "current.5v", "5v rail current", "milliamps"},
 		{"max6650-i2c-0-48", "fan1", "fan.fpga", "fpga fan", "rpm"},
@@ -55,25 +56,26 @@ static struct katcl_line *k = NULL;
 static char *app = "roach2hwmon";
 
 /*
-static int getAttributes(const char *chip, char *name, char *sensor,
-							char *description, char *unit)
+static int getAttributes(const char *chip, char *name, const char *sensor,
+							conct char *description, const char *unit)
 {
 	int i = 0;
 
 	while (table[i].chip != NULL) {
-		if (!(strcmp(chip, table[i].chip) && strcmp(name, table[i].name))) {
-			sensor = table[i].sensor;
-			//strncpy(sensor, table[i].sensor, strlen());
-			description = table[i].desc;
-			unit = table[i].unit;
-			return 0;
+        if (strcmp(chip, table[i].chip) == 0) {
+            if (strcmp(name, table[i].name) == 0) {
+			    sensor = table[i].sensor;
+			    description = table[i].desc;
+			    unit = table[i].unit;
+			    return 0;
+            }
 		}
 		i++;
 	}
 
-	sensor = name;
-	description = "no description";
-	unit = "unspecified";
+	sensor = NULL;
+	description = NULL;
+	unit = NULL;
 
 	return 1;
 }
@@ -92,7 +94,7 @@ static const char *getSensor(const char *chip, char *name)
 		i++;
 	}
 
-	return "sensor";
+	return NULL;
 }
 
 static const char *getDescription(const char *chip, char *name)
@@ -108,7 +110,7 @@ static const char *getDescription(const char *chip, char *name)
 		i++;
 	}
 
-	return "no description";
+	return NULL;
 }
 
 static const char *getUnit(const char *chip, char *name)
@@ -124,7 +126,7 @@ static const char *getUnit(const char *chip, char *name)
 		i++;
 	}
 
-	return "unspecified";
+	return NULL;
 }
 
 int log_init(void)
@@ -162,35 +164,12 @@ int log_message(int loglevel, char *fmt, ...)
     return ret;
 }
 
-int log_sensorlist(void)
-{
-	int ret = 0;
-
-	if (k == NULL) {
-		return -1;
-	}
-
-	/* register sensor(s) */
-	ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-list");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "dummytemp");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "dummy sensor temperature");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "millidegrees");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "integer");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "0");
-	ret += append_string_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_STRING, "80000");
-
-    /* write the log data out */
-    while ((ret = write_katcl(k)) == 0);
-
-	return ret;
-}
-
 int log_addsensor(const char *chip, char *name, double min, double max)
 {
 	int ret = 0;
-	//char namebuff[256];
-	//char descbuff[256];
-	//char unitbuff[256];
+	const char *sensorName = getSensor(chip, name);
+	const char *sensorDesc = getDescription(chip, name);
+	const char *sensorUnit = getUnit(chip, name);
 
 	if (k == NULL) {
 		return -1;
@@ -200,75 +179,61 @@ int log_addsensor(const char *chip, char *name, double min, double max)
 	//getAttributes(chip, name, namebuff, descbuff, unitbuff);
 
 	/* register sensor(s) */
-	ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-list");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getSensor(chip, name));
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getDescription(chip, name));
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getUnit(chip, name));
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "integer");
+    if (sensorName && sensorDesc && sensorUnit) {
+	    ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-list");
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorName);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorDesc);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorUnit);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, "integer");
 
-	/* multiply with a 1000 since our units are in milli's */
-	ret += append_signed_long_katcl(k, KATCP_FLAG_SLONG, (long)(min*1000));
-	ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(max*1000));
+	    /* multiply with a 1000 since our units are in milli's */
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_SLONG, (long)(min*1000));
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(max*1000));
 
-    /* write the log data out */
-    while ((ret = write_katcl(k)) == 0);
-
-	return ret;
-}
-
-int log_sensorstatus(void)
-{
-	int ret = 0;
-
-	if (k == NULL) {
-		return -1;
-	}
-
-	/* update sensor status */
-	ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-status");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "123456789");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "1");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "dummytemp");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "nominal");
-	ret += append_string_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_STRING, "23000");
-
-    /* write the log data out */
-    while ((ret = write_katcl(k)) == 0);
+        /* write the log data out */
+        while ((ret = write_katcl(k)) == 0);
+    }
 
 	return ret;
 }
 
 int log_update_sensor(const char *chip, char *name, int status, double val)
 {
-	int ret = 0;
+	struct timeval now;
+	const char *sensorName = getSensor(chip, name);
+    int ret = 0;
 
 	if (k == NULL) {
 		return -1;
 	}
 
-	/* update sensor status */
-	ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-status");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "123456789");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, "1");
-	ret += append_string_katcl(k, KATCP_FLAG_STRING, name);
+    if (sensorName) {
+        gettimeofday(&now, NULL);
 
-	if (status == KATCP_LEVEL_INFO) {
-		ret += append_string_katcl(k, KATCP_FLAG_STRING, "nominal");
-	} else if (status == KATCP_LEVEL_WARN){
-		ret += append_string_katcl(k, KATCP_FLAG_STRING, "warn");
-	} else if (status == KATCP_LEVEL_ERROR){
-		ret += append_string_katcl(k, KATCP_FLAG_STRING, "error");
-	} else if (status == KATCP_LEVEL_FATAL){
-		ret += append_string_katcl(k, KATCP_FLAG_STRING, "failure");
-	} else {
-		ret += append_string_katcl(k, KATCP_FLAG_STRING, "unknown");
-	}
+	    /* update sensor status */
+	    ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, "#sensor-status");
+        ret += append_unsigned_long_katcl(k, KATCP_FLAG_ULONG, (unsigned long)now.tv_sec);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, "1");
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorName);
 
-	/* multiply with a 1000 since our units are in milli's */
-	ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(val*1000));
+	    if (status == KATCP_LEVEL_INFO) {
+		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "nominal");
+	    } else if (status == KATCP_LEVEL_WARN){
+		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "warn");
+	    } else if (status == KATCP_LEVEL_ERROR){
+		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "error");
+	    } else if (status == KATCP_LEVEL_FATAL){
+		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "failure");
+	    } else {
+		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "unknown");
+	    }
 
-    /* write the log data out */
-    while ((ret = write_katcl(k)) == 0);
+	    /* multiply with a 1000 since our units are in milli's */
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(val*1000));
+
+        /* write the log data out */
+        while ((ret = write_katcl(k)) == 0);
+    }
 
 	return ret;
 }
