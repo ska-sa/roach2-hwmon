48e703e61caafb90135c179fb9017592601ba293
Rijan de Nysschen <rdenysschen@ska.ac.za>
2014-06-04 12:40:53 +0200

    Added scaling factors to sensor table. Improved logic by making use of an index to access elements from the sensor table.

--- log.c
+++ log.c
@@ -17,38 +17,40 @@ struct chipLookup {
 	const char *sensor;
 	const char *desc;
 	const char *unit;
+	float mult;
+	float div;
 };
 
 static const struct chipLookup table[] = {
 		/* Adding sensor mappings
            {"chip1", "name1", "sensor1", "desc1", "unit1"},
 		   {"chip2", "name2", "sensor2", "desc2", "unit2"}, */
-		{"acpitz-virtual-0", "temp1", "hp.temp", "hp temp sensor", "milidegrees"},
-		{"ad7414-i2c-0-4c", "temp1", "temp.inlet", "inlet temp", "millidegrees"},
-		{"ad7414-i2c-0-4e", "temp1", "temp.outlet", "outlet temp", "millidegrees"},
-		{"max1668-i2c-0-18", "temp1", "temp.ambient", "ambient temp", "millidegrees"},
-		{"max1668-i2c-0-18", "temp2", "temp.ppc", "ppc temp", "millidegrees"},
-		{"max1668-i2c-0-18", "temp3", "temp.fpga", "fpga temp", "millidegrees"},
-		{"max16071-i2c-0-50", "in0", "voltage.1v0", "1v voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in1", "voltage.1v5", "1v5 voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in2", "voltage.1v8", "1v8 voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in3", "voltage.2v5", "2v5 voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in4", "voltage.3v3", "3v3 voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in5", "voltage.5v", "5v voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in6", "voltage.12v", "12v voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "in7", "voltage.3v3aux", "3v3 aux voltage rail", "millivolts"},
-		{"max16071-i2c-0-50", "curr1", "current.12v", "12v rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in0", "current.3v3", "1v rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in1", "current.2v5", "1v5 rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in2", "current.1v8", "1v8 rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in3", "current.1v5", "2v5 rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in4", "current.1v0", "3v3 rail current", "milliamps"},
-		{"max16071-i2c-0-51", "in6", "voltage.5vaux", "5v aux voltage rail", "millivolts"},
-		{"max16071-i2c-0-51", "curr1", "current.5v", "5v rail current", "milliamps"},
-		{"max6650-i2c-0-48", "fan1", "fan.fpga", "fpga fan", "rpm"},
-		{"max6650-i2c-0-4b", "fan1", "fan.chs0", "chassis 0 fan", "rpm"},
-		{"max6650-i2c-0-1b", "fan1", "fan.chs1", "chassis 1 fan", "rpm"},
-		{"max6650-i2c-0-1f", "fan1", "fan.chs2", "chassis 2 fan", "rpm"},
+		{"acpitz-virtual-0", "temp1", "hp.temp", "hp temp sensor", "milidegrees", 1, 1},
+		{"ad7414-i2c-0-4c", "temp1", "temp.inlet", "inlet temp", "millidegrees", 1, 1},
+		{"ad7414-i2c-0-4e", "temp1", "temp.outlet", "outlet temp", "millidegrees", 1, 1},
+		{"max1668-i2c-0-18", "temp1", "temp.ambient", "ambient temp", "millidegrees", 1, 1},
+		{"max1668-i2c-0-18", "temp2", "temp.ppc", "ppc temp", "millidegrees", 1, 1},
+		{"max1668-i2c-0-18", "temp3", "temp.fpga", "fpga temp", "millidegrees", 1, 1},
+		{"max16071-i2c-0-50", "in0", "voltage.1v0", "1v voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in1", "voltage.1v5", "1v5 voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in2", "voltage.1v8", "1v8 voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in3", "voltage.2v5", "2v5 voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in4", "voltage.3v3", "3v3 voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in5", "voltage.5v", "5v voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "in6", "voltage.12v", "12v voltage rail", "millivolts", 10, 29.6},
+		{"max16071-i2c-0-50", "in7", "voltage.3v3aux", "3v3 aux voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-50", "curr1", "current.12v", "12v rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in0", "current.3v3", "1v rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in1", "current.2v5", "1v5 rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in2", "current.1v8", "1v8 rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in3", "current.1v5", "2v5 rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in4", "current.1v0", "3v3 rail current", "milliamps", 1, 1},
+		{"max16071-i2c-0-51", "in6", "voltage.5vaux", "5v aux voltage rail", "millivolts", 1, 1},
+		{"max16071-i2c-0-51", "curr1", "current.5v", "5v rail current", "milliamps", 1, 1},
+		{"max6650-i2c-0-48", "fan1", "fan.fpga", "fpga fan", "rpm", 1, 1},
+		{"max6650-i2c-0-4b", "fan1", "fan.chs0", "chassis 0 fan", "rpm", 1, 1},
+		{"max6650-i2c-0-1b", "fan1", "fan.chs1", "chassis 1 fan", "rpm", 1, 1},
+		{"max6650-i2c-0-1f", "fan1", "fan.chs2", "chassis 2 fan", "rpm", 1, 1},
 		{NULL, NULL, NULL, NULL, NULL}
 };
 
@@ -80,53 +82,65 @@ static int getAttributes(const char *chip, char *name, const char *sensor,
 	return 1;
 }
 */
-
-static const char *getSensor(const char *chip, char *name)
+static int getSensorIndex(const char *chip, char *name)
 {
 	int i = 0;
 
 	while (table[i].chip != NULL) {
 		if (strcmp(chip, table[i].chip) == 0) {
 			if (strcmp(name, table[i].name) == 0) {
-				return table[i].sensor;
+				return i;
 			}
 		}
 		i++;
+	}
+
+	return -1;
+}
+
+static const char *getSensor(int index)
+{
+	if (index >= 0) {
+		return table[index].sensor;
 	}
 
 	return NULL;
 }
 
-static const char *getDescription(const char *chip, char *name)
+static const char *getDescription(int index)
 {
-	int i = 0;
-
-	while (table[i].chip != NULL) {
-		if (strcmp(chip, table[i].chip) == 0) {
-			if (strcmp(name, table[i].name) == 0) {
-				return table[i].desc;
-			}
-		}
-		i++;
+	if (index >= 0) {
+		return table[index].desc;
 	}
 
 	return NULL;
 }
 
-static const char *getUnit(const char *chip, char *name)
+static const char *getUnit(int index)
 {
-	int i = 0;
-
-	while (table[i].chip != NULL) {
-		if (strcmp(chip, table[i].chip) == 0) {
-			if (strcmp(name, table[i].name) == 0) {
-				return table[i].unit;
-			}
-		}
-		i++;
+	if (index >= 0) {
+		return table[index].unit;
 	}
 
 	return NULL;
+}
+
+static float getMult(int index)
+{
+	if (index >= 0) {
+		return table[index].mult;
+	}
+
+	return 1;
+}
+
+static float getDiv(int index)
+{
+	if (index >= 0) {
+		return table[index].div;
+	}
+
+	return 1;
 }
 
 int log_init(void)
@@ -166,33 +180,35 @@ int log_message(int loglevel, char *fmt, ...)
 
 int log_addsensor(const char *chip, char *name, double min, double max)
 {
+	long minVal = 0, maxVal = 0;
+	int sensorIndex = -1;
 	int ret = 0;
-	const char *sensorName = getSensor(chip, name);
-	const char *sensorDesc = getDescription(chip, name);
-	const char *sensorUnit = getUnit(chip, name);
 
 	if (k == NULL) {
 		return -1;
 	}
 
-	/* populate sensor attributes */
-	//getAttributes(chip, name, namebuff, descbuff, unitbuff);
+	/* check if the sensor exists in the table */
+	sensorIndex = getSensorIndex(chip, name);
 
 	/* register sensor(s) */
-    if (sensorName && sensorDesc && sensorUnit) {
+	if (sensorIndex >= 0) {
 	    ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, KATCP_SENSOR_LIST_INFORM);
-	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorName);
-	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorDesc);
-	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorUnit);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getSensor(sensorIndex));
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getDescription(sensorIndex));
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getUnit(sensorIndex));
 	    ret += append_string_katcl(k, KATCP_FLAG_STRING, "integer");
 
 	    /* multiply with a 1000 since our units are in milli's */
-	    ret += append_signed_long_katcl(k, KATCP_FLAG_SLONG, (long)(min*1000));
-	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(max*1000));
+	    minVal = (min * 1000) * getMult(sensorIndex) / getDiv(sensorIndex);
+	    maxVal = (max * 1000) * getMult(sensorIndex) / getDiv(sensorIndex);
+
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_SLONG, minVal);
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, maxVal);
 
         /* write the log data out */
         while ((ret = write_katcl(k)) == 0);
-    }
+	}
 
 	return ret;
 }
@@ -200,19 +216,22 @@ int log_addsensor(const char *chip, char *name, double min, double max)
 int log_update_sensor(const char *chip, char *name, int status, double val)
 {
 	struct timeval now;
-	const char *sensorName = getSensor(chip, name);
+	int sensorIndex = -1;
+	long value = 0;
     int ret = 0;
 
 	if (k == NULL) {
 		return -1;
 	}
 
-    if (sensorName) {
+	/* check if the sensor exists in the table */
+	sensorIndex = getSensorIndex(chip, name);
+
+	if (sensorIndex >= 0) {
         gettimeofday(&now, NULL);
 
 	    /* update sensor status */
 	    ret += append_string_katcl(k, KATCP_FLAG_FIRST | KATCP_FLAG_STRING, KATCP_SENSOR_STATUS_INFORM);
-
         ret += append_unsigned_long_katcl(k, KATCP_FLAG_ULONG, (unsigned long)now.tv_sec);
 /*
 #if KATCP_PROTOCOL_MAJOR_VERSION >= 5
@@ -222,7 +241,7 @@ int log_update_sensor(const char *chip, char *name, int status, double val)
 #endif
 */
 	    ret += append_string_katcl(k, KATCP_FLAG_STRING, "1");
-	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)sensorName);
+	    ret += append_string_katcl(k, KATCP_FLAG_STRING, (char*)getSensor(sensorIndex));
 
 	    if (status == KATCP_STATUS_NOMINAL) {
 		    ret += append_string_katcl(k, KATCP_FLAG_STRING, "nominal");
@@ -237,7 +256,8 @@ int log_update_sensor(const char *chip, char *name, int status, double val)
 	    }
 
 	    /* multiply with a 1000 since our units are in milli's */
-	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, (long)(val*1000));
+	    value = (long)((val * 1000) * getMult(sensorIndex) / getDiv(sensorIndex));
+	    ret += append_signed_long_katcl(k, KATCP_FLAG_LAST | KATCP_FLAG_SLONG, value);
 
         /* write the log data out */
         while ((ret = write_katcl(k)) == 0);
